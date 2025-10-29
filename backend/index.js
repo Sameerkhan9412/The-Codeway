1575a2b7e6e958d4470539106ea6afff947d652d
sameerkhan9412 <sameerkhann9412@gmail.com>
2025-10-29 11:18:39 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -27,12 +27,14 @@ const videoRouter = require("./routes/video");
 
 const PORT_NO = process.env.PORT_NO;
 
-app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// app.use(cors({
+//   origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
+app.options('*', cors()); // enable pre-flight across all routes
 
 
 app.use(express.json());
