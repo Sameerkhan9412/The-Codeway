349798c60a65c688f5efe1cc34f5c02cc24c0fc0
sameerkhan9412 <sameerkhann9412@gmail.com>
2025-10-29 11:06:36 +0530

    again solve cors problem

--- backend/index.js
+++ backend/index.js
@@ -28,9 +28,12 @@ const videoRouter = require("./routes/video");
 const PORT_NO = process.env.PORT_NO;
 
 app.use(cors({
-  origin: '*',
-  credentials: true
-}))
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
