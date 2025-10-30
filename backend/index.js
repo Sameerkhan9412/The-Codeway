c25f7b1cd19e051697389a334b382c9fd020fba6
sameerkhan9412 <sameerkhann9412@gmail.com>
2025-10-30 12:02:20 +0530

    merge conflict resolve

--- backend/index.js
+++ backend/index.js
@@ -28,14 +28,18 @@ const videoRouter = require("./routes/video");
 const PORT_NO = process.env.PORT_NO;
 
 // app.use(cors({
-//   origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
+//   origin: [process.env.FRONTEND_URL],
+//   credentials: true
+// }))
 
-app.options('*', cors()); // enable pre-flight across all routes
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://the-codeway.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -59,6 +63,10 @@ app.use("/api/contest", contestRouter)
 app.use('/api/playlists', playlistRouter);
 app.use('/api/discussions', discussionRouter);
 app.use("/api/dsa", dsaRouter);
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+});
+
 
 const initialConnection = async () => {
     try {
